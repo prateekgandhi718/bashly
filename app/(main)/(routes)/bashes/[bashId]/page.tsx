592b0f3bd415de656f7a3e91c5f1793606c04d00
Prateek <prateekgandhi718@gmail.com>
2024-04-10 02:16:31 +0530

    add linting

--- app/(main)/(routes)/bashes/[bashId]/page.tsx
+++ app/(main)/(routes)/bashes/[bashId]/page.tsx
@@ -1,6 +1,6 @@
 const BashIdPage = () => {
   return (
-    <div>Bash ID Page. On this page we will show the event's start and end date. We can build a form and save the data and then display the data. But the side nav mobile is in chat header so make sure to have that here.</div>
+    <div>Bash ID Page. On this page we will show the start date and end date of bash. We can build a form and save the data and then display the data. But the side nav mobile is in chat header so make sure to have that here.</div>
   )
 }
 
