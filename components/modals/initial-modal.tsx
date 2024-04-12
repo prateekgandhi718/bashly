171a2f668535b399fbd95c4484ed31c40c1c9edf
Prateek <prateekgandhi718@gmail.com>
2024-04-12 23:26:01 +0530

    fix modal size on mobile

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -88,7 +88,7 @@ const InitialModal = () => {
   return (
     <Dialog open>
       <DialogContent className="bg-white dark:bg-[#313338] text-black dark:text-zinc-50 p-0 overflow-hidden">
-        <DialogHeader className="pt-8 px-6">
+      <DialogHeader className="pt-6 px-6">
           <DialogTitle className="text-2xl text-center">
             Create your Bash
           </DialogTitle>
@@ -98,7 +98,7 @@ const InitialModal = () => {
           </DialogDescription>
         </DialogHeader>
         <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2">
             <div className="space-y-1 px-6 flex flex-col justify-center items-center gap-2">
               <div className="flex items-center justify-center text-center">
                 <FormField
@@ -144,7 +144,7 @@ const InitialModal = () => {
                   name="startDate"
                   render={({ field }) => (
                     <FormItem className="flex flex-col">
-                      <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">Start date</FormLabel>
+                      <FormLabel className="uppercase text-xs font-bold text-zinc-500">Start date</FormLabel>
                       <Popover>
                         <PopoverTrigger asChild>
                           <FormControl>
@@ -182,7 +182,7 @@ const InitialModal = () => {
                   name="endDate"
                   render={({ field }) => (
                     <FormItem className="flex flex-col">
-                      <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">End date</FormLabel>
+                      <FormLabel className="uppercase text-xs font-bold text-zinc-500">End date</FormLabel>
                       <Popover>
                         <PopoverTrigger asChild>
                           <FormControl>
